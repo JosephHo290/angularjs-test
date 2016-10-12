8d383c740037a55a317d47568b602f9c226e3745
JosephHo290 <JosephHo290@yahoo.com.tw>
2016-10-12 18:04:15 +0800

    Final

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,26 +12,21 @@ function lunchCheckController ($scope) {
   */
   
   $scope.chkAndShow = function () {
-  	var arr = $scope.textin.split(',');
+	if ($scope.textin == undefined || $scope.textin == ""){
+		$scope.msg = "Please enter data first";
+	}  else {
+		
+	var arr = $scope.textin.split(',');
 
-  	$scope.style = {};
-    
-  	if ($scope.textin == "") {
-  		$scope.msg = "Please enter data first";
-  		$scope.style.textColor = 'red';
-  		$scope.style.validation = 'error';
-  	}
-  	else if (arr.length > 3) {
+  	//console.log("Ho:" + $scope.textin);
+	 
+  	if (arr.length > 3) {
   		$scope.msg = "Too much!";
-      $scope.style.textColor = 'green';
-      $scope.style.validation = 'valid';
-    }	else
+      }	else
   	{
   		$scope.msg = "Enjoy!";
-      $scope.style.textColor = 'green';
-      $scope.style.validation = 'valid'; 
-  	}
-    
+   	}
+   }
   };
 };
   
